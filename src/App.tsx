af1480d06f83bc70404cdcbb47362dd192042e94
Sam Magura <srmagura@gmail.com>
2025-05-31 14:18:16 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
